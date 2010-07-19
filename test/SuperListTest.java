92ac68e55be954d2d08919693f0981af9cb470ff
Daniel Schäufele <git@schaeufele.org>
2010-07-19 18:33:19 +0200

    test stubs added

---
+++ test/SuperListTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SuperListTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAddT() {
+		fail("Not yet implemented");
+	}
+
+}
