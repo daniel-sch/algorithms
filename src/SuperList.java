7d663400a2be98681ce9f835d4e84c7d5cac8e2d
Daniel Schäufele <git@schaeufele.org>
2010-07-21 22:52:14 +0200

    * implemented Deque Interface * Sorted Members etc.

--- src/SuperList.java
+++ src/SuperList.java
@@ -1,12 +1,146 @@
 import java.util.AbstractSequentialList;
 import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
 import java.util.Queue;
 
 public class SuperList<T extends Comparable<T>> extends
-		AbstractSequentialList<T> implements List<T>, Queue<T> {
+		AbstractSequentialList<T> implements List<T>, Queue<T>, Deque<T> {
+	private class Item {
+		T data;
+		Item next;
+		Item prev;
+
+		Item(Item prev, Item next, T data) {
+			this.prev = prev;
+			this.next = next;
+			this.data = data;
+		}
+	}
+
+	private class SuperListDescendingIterator implements Iterator<T> {
+		private Item cur;
+		private Item last;
+		private SuperList<T> list;
+
+		public SuperListDescendingIterator(Item i, SuperList<T> l) {
+			cur = i;
+			list = l;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return cur != null;
+		}
+
+		@Override
+		public T next() {
+			if (cur == null)
+				throw new NoSuchElementException();
+			T d = cur.data;
+			last = cur;
+			cur = cur.prev;
+			return d;
+		}
+
+		@Override
+		public void remove() {
+			if (last == null)
+				throw new IllegalStateException();
+			list.removeItem(last);
+			last = null;
+		}
+
+	}
+
+	private class SuperListIterator implements ListIterator<T> {
+		private Item cur;
+		private int index;
+		private Item last;
+		private SuperList<T> list;
+
+		public SuperListIterator(Item i, int index, SuperList<T> l) {
+			cur = i;
+			this.index = index;
+			list = l;
+		}
+
+		@Override
+		public void add(T e) {
+			last = null;
+			list.addItemBefore(cur, e);
+			index++;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return cur != null;
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			if (cur == null && list.size() > 0)
+				return true;
+			return cur.prev != null;
+		}
+
+		@Override
+		public T next() {
+			if (cur == null)
+				throw new NoSuchElementException();
+
+			index++;
+			T d = cur.data;
+			last = cur;
+			cur = cur.next;
+			return d;
+		}
+
+		@Override
+		public int nextIndex() {
+			return index + 1;
+		}
+
+		@Override
+		public T previous() {
+			if (cur == null)
+				cur = list.mLast;
+			else
+				cur = cur.prev;
+
+			if (cur == null)
+				throw new NoSuchElementException();
+
+			index--;
+			last = cur;
+			return cur.data;
+		}
+
+		@Override
+		public int previousIndex() {
+			return index - 1;
+		}
+
+		@Override
+		public void remove() {
+			if (last == null)
+				throw new IllegalStateException();
+			list.removeItem(last);
+			last = null;
+			index -= 2;
+		}
+
+		@Override
+		public void set(T e) {
+			if (last == null)
+				throw new IllegalStateException();
+			last.data = e;
+		}
+	}
+
 	private Item mFirst;
 	private Item mLast;
 	private int mSize;
@@ -25,9 +159,43 @@ public class SuperList<T extends Comparable<T>> extends
 	}
 
 	@Override
+	public void addFirst(T e) {
+		addItemBefore(mFirst, e);
+	}
+
+	@Override
+	public void addLast(T e) {
+		add(e);
+	}
+
+	@Override
 	public void clear() {
 		mSize = 0;
 		mFirst = mLast = null;
+	}
+
+	@Override
+	public Iterator<T> descendingIterator() {
+		return new SuperListDescendingIterator(mLast, this);
+	}
+
+	@Override
+	public T element() {
+		if (mSize == 0)
+			throw new NoSuchElementException();
+		return mFirst.data;
+	}
+
+	@Override
+	public T getFirst() {
+		return element();
+	}
+
+	@Override
+	public T getLast() {
+		if (mSize == 0)
+			throw new NoSuchElementException();
+		return mLast.data;
 	}
 
 	@Override
@@ -54,20 +222,19 @@ public class SuperList<T extends Comparable<T>> extends
 	}
 
 	@Override
-	public int size() {
-		return mSize;
-	}
-
-	@Override
-	public T element() {
-		if (mSize == 0)
-			throw new NoSuchElementException();
-		return mFirst.data;
-	}
-
-	@Override
 	public boolean offer(T e) {
 		return add(e);
+	}
+
+	@Override
+	public boolean offerFirst(T e) {
+		addFirst(e);
+		return true;
+	}
+
+	@Override
+	public boolean offerLast(T e) {
+		return offer(e);
 	}
 
 	@Override
@@ -75,6 +242,18 @@ public class SuperList<T extends Comparable<T>> extends
 		if (mSize == 0)
 			return null;
 		return mFirst.data;
+	}
+
+	@Override
+	public T peekFirst() {
+		return peek();
+	}
+
+	@Override
+	public T peekLast() {
+		if (mSize == 0)
+			return null;
+		return mLast.data;
 	}
 
 	@Override
@@ -87,10 +266,87 @@ public class SuperList<T extends Comparable<T>> extends
 	}
 
 	@Override
+	public T pollFirst() {
+		return poll();
+	}
+
+	@Override
+	public T pollLast() {
+		if (mSize == 0)
+			return null;
+		T d = mLast.data;
+		removeItem(mLast);
+		return d;
+	}
+
+	@Override
+	public T pop() {
+		return removeFirst();
+	}
+
+	@Override
+	public void push(T e) {
+		addFirst(e);
+	}
+
+	@Override
 	public T remove() {
 		if (mSize == 0)
 			throw new NoSuchElementException();
 		return poll();
+	}
+
+	@Override
+	public T removeFirst() {
+		return remove();
+	}
+
+	@Override
+	public boolean removeFirstOccurrence(Object o) {
+		return remove(o);
+	}
+
+	@Override
+	public T removeLast() {
+		if (mSize == 0)
+			throw new NoSuchElementException();
+		T d = mLast.data;
+		removeItem(mLast);
+		return d;
+	}
+
+	@Override
+	public boolean removeLastOccurrence(Object o) {
+		Iterator<T> i = descendingIterator();
+		while (i.hasNext()) {
+			T e = i.next();
+			if (o == null ? e == null : o.equals(e)) {
+				i.remove();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int size() {
+		return mSize;
+	}
+
+	private void addItemBefore(Item o, T element) {
+		if (o == null) {
+			if (mSize == 0)
+				mLast = mFirst = new Item(null, null, element);
+			else
+				mLast = mLast.next = new Item(mLast, null, element);
+		} else if (o.prev == null)
+			mFirst = mFirst.prev = new Item(null, mFirst, element);
+		else {
+			Item n = new Item(o.prev, o, element);
+			o.prev.next = n;
+			o.prev = n;
+		}
+		mSize++;
 	}
 
 	private Item getItem(int index) {
@@ -115,114 +371,10 @@ public class SuperList<T extends Comparable<T>> extends
 		mSize--;
 	}
 
-	private void addItemBefore(Item o, T element) {
-		if (o == null) {
-			if (mSize == 0)
-				mLast = mFirst = new Item(null, null, element);
-			else
-				mLast = mLast.next = new Item(mLast, null, element);
-		} else if (o.prev == null)
-			mFirst = mFirst.prev = new Item(null, mFirst, element);
-		else {
-			Item n = new Item(o.prev, o, element);
-			o.prev.next = n;
-			o.prev = n;
-		}
-		mSize++;
-	}
-
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		List<T> l = new SuperList<T>();
 		l.addAll(this);
 		return l;
-	}
-
-	private class Item {
-		Item prev;
-		Item next;
-		T data;
-
-		Item(Item prev, Item next, T data) {
-			this.prev = prev;
-			this.next = next;
-			this.data = data;
-		}
-	}
-
-	private class SuperListIterator implements ListIterator<T> {
-		private Item cur;
-		private SuperList<T> list;
-		private int index;
-		private Item last;
-
-		public SuperListIterator(Item i, int index, SuperList<T> l) {
-			cur = i;
-			this.index = index;
-			list = l;
-		}
-
-		public void add(T e) {
-			last = null;
-			list.addItemBefore(cur, e);
-			index++;
-		}
-
-		public boolean hasNext() {
-			return cur != null;
-		}
-
-		public boolean hasPrevious() {
-			if (cur == null && list.size() > 0)
-				return true;
-			return cur.prev != null;
-		}
-
-		public T next() {
-			if (cur == null)
-				throw new NoSuchElementException();
-
-			index++;
-			T d = cur.data;
-			last = cur;
-			cur = cur.next;
-			return d;
-		}
-
-		public int nextIndex() {
-			return index + 1;
-		}
-
-		public T previous() {
-			if (cur == null)
-				cur = list.mLast;
-			else
-				cur = cur.prev;
-
-			if (cur == null)
-				throw new NoSuchElementException();
-
-			index--;
-			last = cur;
-			return cur.data;
-		}
-
-		public int previousIndex() {
-			return index - 1;
-		}
-
-		public void remove() {
-			if (last == null)
-				throw new IllegalStateException();
-			list.removeItem(last);
-			last = null;
-			index -= 2;
-		}
-
-		public void set(T e) {
-			if (last == null)
-				throw new IllegalStateException();
-			last.data = e;
-		}
 	}
 }
--- test/SuperListTest.java
+++ test/SuperListTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
@@ -14,37 +15,6 @@ public class SuperListTest {
 	@Before
 	public void setUp() throws Exception {
 		l = new SuperList<String>();
-	}
-
-	@Test
-	public void testSizeIsNullAfterCreate() {
-		assertEquals(0, l.size());
-	}
-
-	@Test
-	public void testCanAdd() {
-		assertTrue(l.add("foo"));
-		assertEquals("foo", l.get(0));
-		assertEquals(1, l.size());
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void testThrowsIndexOutOfBoundsExceptionOnGet() {
-		l.get(1);
-	}
-
-	@Test
-	public void testCanAddMultiple() {
-		assertTrue(l.add("foo"));
-		assertTrue(l.add("bar"));
-		assertEquals("foo", l.get(0));
-		assertEquals("bar", l.get(1));
-		assertEquals(2, l.size());
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void testAdd2ThrowsIndexOutOfBoundsException() {
-		l.add(1, "foo");
 	}
 
 	@Test
@@ -63,6 +33,65 @@ public class SuperListTest {
 		assertEquals(3, l.size());
 	}
 
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testAdd2ThrowsIndexOutOfBoundsException() {
+		l.add(1, "foo");
+	}
+
+	@Test
+	public void testAddAllWithIndex() {
+		l.add("a");
+		l.add("d");
+		List<String> c = new ArrayList<String>();
+		c.add("b");
+		c.add("c");
+		assertTrue(l.addAll(1, c));
+		assertEquals(4, l.size());
+		assertEquals("a", l.get(0));
+		assertEquals("b", l.get(1));
+		assertEquals("c", l.get(2));
+		assertEquals("d", l.get(3));
+		assertEquals(4, l.size());
+	}
+
+	@Test
+	public void testAddAllWithoutIndex() {
+		l.add("a");
+		List<String> c = new ArrayList<String>();
+		c.add("b");
+		c.add("c");
+		assertTrue(l.addAll(c));
+		assertEquals(3, l.size());
+		assertEquals("a", l.get(0));
+		assertEquals("b", l.get(1));
+		assertEquals("c", l.get(2));
+	}
+
+	@Test
+	public void testAddFirst() {
+		l.add("a");
+		l.addFirst("b");
+		assertEquals("b", l.get(0));
+		assertEquals("a", l.get(1));
+		assertEquals(2, l.size());
+	}
+
+	@Test
+	public void testCanAdd() {
+		assertTrue(l.add("foo"));
+		assertEquals("foo", l.get(0));
+		assertEquals(1, l.size());
+	}
+
+	@Test
+	public void testCanAddMultiple() {
+		assertTrue(l.add("foo"));
+		assertTrue(l.add("bar"));
+		assertEquals("foo", l.get(0));
+		assertEquals("bar", l.get(1));
+		assertEquals(2, l.size());
+	}
+
 	@Test
 	public void testClear() {
 		l.add("foo");
@@ -70,38 +99,21 @@ public class SuperListTest {
 		assertEquals(0, l.size());
 	}
 
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testClone() throws CloneNotSupportedException {
+		l.add("a");
+		l.add("b");
+		SuperList<String> l2 = (SuperList<String>) l.clone();
+		l2.add("c");
+		assertEquals(2, l.size());
+	}
+
 	@Test
 	public void testContains() {
 		l.add("foo");
 		assertFalse(l.contains("bar"));
 		assertTrue(l.contains("foo"));
-	}
-
-	@Test
-	public void testIsEmpty() {
-		assertTrue(l.isEmpty());
-		l.add("foo");
-		assertFalse(l.isEmpty());
-	}
-
-	@Test
-	public void testIndexOf() {
-		l.add("foo");
-		l.add("bar");
-		l.add("foo");
-		assertEquals(0, l.indexOf("foo"));
-		assertEquals(1, l.indexOf("bar"));
-		assertEquals(-1, l.indexOf("foobar"));
-	}
-
-	@Test
-	public void testLastIndexOf() {
-		l.add("foo");
-		l.add("bar");
-		l.add("foo");
-		assertEquals(2, l.lastIndexOf("foo"));
-		assertEquals(1, l.lastIndexOf("bar"));
-		assertEquals(-1, l.lastIndexOf("foobar"));
 	}
 
 	@Test
@@ -115,6 +127,341 @@ public class SuperListTest {
 		assertTrue(l.containsAll(c));
 		c.add("d");
 		assertFalse(l.containsAll(c));
+	}
+
+	@Test
+	public void testDescendingIterator() {
+		l.add("a");
+		l.add("b");
+		Iterator<String> i = l.descendingIterator();
+		assertTrue(i.hasNext());
+		assertEquals("b", i.next());
+		assertTrue(i.hasNext());
+		assertEquals("a", i.next());
+		assertFalse(i.hasNext());
+		try {
+			i.next();
+			fail("i.next() should throw Exception!");
+		} catch (NoSuchElementException e) {
+		}
+	}
+
+	@Test
+	public void testDescendingIteratorRemove() {
+		l.add("a");
+		l.add("b");
+		Iterator<String> i = l.descendingIterator();
+		try {
+			i.remove();
+			fail("i.remove should throw Exception!");
+		} catch (IllegalStateException e) {
+		}
+		i.next();
+		i.remove();
+		assertEquals("a", l.get(0));
+		assertEquals(1, l.size());
+		i.next();
+		i.remove();
+		assertTrue(l.isEmpty());
+	}
+
+	@Test
+	public void testElement() {
+		try {
+			l.element();
+			fail("l.element() should throw excepton!");
+		} catch (NoSuchElementException e) {
+		}
+		l.add("a");
+		l.add("b");
+		assertEquals("a", l.element());
+	}
+
+	@Test
+	public void testEquals() {
+		l.add("a");
+		l.add("b");
+		l.add("c");
+		List<String> c = new ArrayList<String>();
+		c.add("a");
+		c.add("b");
+		assertFalse(l.equals(c));
+		assertFalse(c.equals(l));
+		c.add("c");
+		assertTrue(l.equals(c));
+		assertTrue(c.equals(l));
+	}
+
+	@Test
+	public void testGetLast() {
+		try {
+			l.getLast();
+			fail("l.getLast should throw exception!");
+		} catch (NoSuchElementException e) {
+		}
+		l.add("a");
+		l.add("b");
+		assertEquals("b", l.getLast());
+	}
+
+	@Test
+	public void testIndexOf() {
+		l.add("foo");
+		l.add("bar");
+		l.add("foo");
+		assertEquals(0, l.indexOf("foo"));
+		assertEquals(1, l.indexOf("bar"));
+		assertEquals(-1, l.indexOf("foobar"));
+	}
+
+	@Test
+	public void testIsEmpty() {
+		assertTrue(l.isEmpty());
+		l.add("foo");
+		assertFalse(l.isEmpty());
+	}
+
+	@Test
+	public void testIteratorAdd() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator();
+		i.add("c");
+		i.next();
+		i.add("d");
+		assertEquals(4, i.nextIndex());
+		assertEquals("d", i.previous());
+		assertEquals("c", l.get(0));
+		assertEquals("a", l.get(1));
+		assertEquals("d", l.get(2));
+		assertEquals("b", l.get(3));
+	}
+
+	@Test
+	public void testIteratorHasNext() {
+		l.add("a");
+		l.add("b");
+		assertTrue(l.listIterator().hasNext());
+		assertTrue(l.listIterator(1).hasNext());
+		assertFalse(l.listIterator(2).hasNext());
+	}
+
+	@Test
+	public void testIteratorHasPrevious() {
+		l.add("a");
+		l.add("b");
+		assertFalse(l.listIterator().hasPrevious());
+		assertTrue(l.listIterator(1).hasPrevious());
+		assertTrue(l.listIterator(2).hasPrevious());
+	}
+
+	@Test
+	public void testIteratorNext() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator();
+		assertEquals("a", i.next());
+		assertTrue(i.hasNext());
+		assertEquals("b", i.next());
+		assertEquals(3, i.nextIndex());
+		assertFalse(i.hasNext());
+		try {
+			i.next();
+			fail("i.next() should throw exception!");
+		} catch (NoSuchElementException e) {
+		}
+	}
+
+	@Test
+	public void testIteratorNextIndex() {
+		l.add("a");
+		l.add("b");
+		assertEquals(1, l.listIterator().nextIndex());
+		assertEquals(2, l.listIterator(1).nextIndex());
+	}
+
+	@Test
+	public void testIteratorNextPrevious() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator(2);
+		assertEquals("b", i.previous());
+		assertTrue(i.hasPrevious());
+		assertEquals("a", i.previous());
+		assertTrue(i.hasNext());
+		assertEquals("a", i.next());
+		assertEquals("b", i.next());
+	}
+
+	@Test
+	public void testIteratorPrevious() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator(2);
+		assertEquals("b", i.previous());
+		assertTrue(i.hasPrevious());
+		assertEquals("a", i.previous());
+		assertEquals(-1, i.previousIndex());
+		assertFalse(i.hasPrevious());
+		try {
+			i.previous();
+			fail("i.previous() schould throw exception!");
+		} catch (NoSuchElementException e) {
+		}
+	}
+
+	@Test
+	public void testIteratorPreviousIndex() {
+		l.add("a");
+		l.add("b");
+		assertEquals(-1, l.listIterator().previousIndex());
+		assertEquals(0, l.listIterator(1).previousIndex());
+		assertEquals(1, l.listIterator(2).previousIndex());
+	}
+
+	@Test
+	public void testIteratorRemove() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator();
+		i.next();
+		i.remove();
+		assertEquals(0, i.nextIndex());
+		assertEquals("b", l.get(0));
+		assertEquals("b", i.next());
+		assertEquals(1, i.nextIndex());
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testIteratorRemoveException() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator();
+		i.remove();
+	}
+
+	@Test
+	public void testIteratorSet() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator();
+		i.next();
+		i.set("c");
+		assertEquals("c", l.get(0));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testIteratorSetException() {
+		l.add("a");
+		l.add("b");
+		ListIterator<String> i = l.listIterator();
+		i.set("c");
+	}
+
+	@Test
+	public void testLastIndexOf() {
+		l.add("foo");
+		l.add("bar");
+		l.add("foo");
+		assertEquals(2, l.lastIndexOf("foo"));
+		assertEquals(1, l.lastIndexOf("bar"));
+		assertEquals(-1, l.lastIndexOf("foobar"));
+	}
+
+	@Test
+	public void testOffer() {
+		assertTrue(l.offer("a"));
+		assertEquals("a", l.get(0));
+		assertEquals(1, l.size());
+	}
+
+	@Test
+	public void testPeek() {
+		assertNull(l.peek());
+		l.add("a");
+		l.add("b");
+		assertEquals("a", l.peek());
+	}
+
+	@Test
+	public void testPeekLast() {
+		assertNull(l.peekLast());
+		l.add("a");
+		l.add("b");
+		assertEquals("b", l.peekLast());
+	}
+
+	@Test
+	public void testPoll() {
+		l.add("a");
+		l.add("b");
+		assertEquals("a", l.poll());
+		assertEquals("b", l.poll());
+		assertNull(l.poll());
+	}
+
+	@Test
+	public void testPollLast() {
+		l.add("a");
+		l.add("b");
+		assertEquals("b", l.pollLast());
+		assertEquals("a", l.pollLast());
+		assertNull(l.pollLast());
+	}
+
+	@Test
+	public void testRemove() {
+		l.add("a");
+		l.add("b");
+		assertEquals("a", l.remove());
+		assertEquals("b", l.remove());
+		try {
+			l.remove();
+		} catch (NoSuchElementException e) {
+		}
+	}
+
+	@Test
+	public void testRemoveAll() {
+		l.add("a");
+		l.add("b");
+		l.add("c");
+		l.add("d");
+		List<String> c = new ArrayList<String>();
+		c.add("a");
+		c.add("c");
+		c.add("e");
+		assertTrue(l.removeAll(c));
+		c = new ArrayList<String>();
+		c.add("f");
+		assertFalse(l.removeAll(c));
+		assertEquals(2, l.size());
+		assertEquals("b", l.get(0));
+		assertEquals("d", l.get(1));
+	}
+
+	@Test
+	public void testRemoveLast() {
+		l.add("a");
+		l.add("b");
+		assertEquals("b", l.removeLast());
+		assertEquals("a", l.removeLast());
+		try {
+			l.removeLast();
+			fail("l.removeLast should throw exception!");
+		} catch (NoSuchElementException e) {
+		}
+	}
+
+	@Test
+	public void testRemoveLastOccurence() {
+		l.add("a");
+		l.add("b");
+		assertFalse(l.removeLastOccurrence("c"));
+		assertEquals(2, l.size());
+		assertTrue(l.removeLastOccurrence("a"));
+		assertEquals(1, l.size());
+		assertEquals("b", l.get(0));
 	}
 
 	@Test
@@ -141,22 +488,19 @@ public class SuperListTest {
 	}
 
 	@Test
-	public void testRemoveAll() {
+	public void testRetainAll() {
 		l.add("a");
 		l.add("b");
 		l.add("c");
 		l.add("d");
 		List<String> c = new ArrayList<String>();
-		c.add("a");
+		c.add("b");
 		c.add("c");
-		c.add("e");
-		assertTrue(l.removeAll(c));
-		c = new ArrayList<String>();
-		c.add("f");
-		assertFalse(l.removeAll(c));
-		assertEquals(2, l.size());
+		assertTrue(l.retainAll(c));
+		assertFalse(l.retainAll(c));
 		assertEquals("b", l.get(0));
-		assertEquals("d", l.get(1));
+		assertEquals("c", l.get(1));
+		assertEquals(2, l.size());
 	}
 
 	@Test
@@ -168,32 +512,28 @@ public class SuperListTest {
 	}
 
 	@Test
-	public void testAddAllWithoutIndex() {
-		l.add("a");
-		List<String> c = new ArrayList<String>();
-		c.add("b");
-		c.add("c");
-		assertTrue(l.addAll(c));
-		assertEquals(3, l.size());
-		assertEquals("a", l.get(0));
-		assertEquals("b", l.get(1));
-		assertEquals("c", l.get(2));
+	public void testSizeIsNullAfterCreate() {
+		assertEquals(0, l.size());
 	}
 
 	@Test
-	public void testAddAllWithIndex() {
+	public void testSublist() {
 		l.add("a");
-		l.add("d");
-		List<String> c = new ArrayList<String>();
-		c.add("b");
-		c.add("c");
-		assertTrue(l.addAll(1, c));
-		assertEquals(4, l.size());
-		assertEquals("a", l.get(0));
-		assertEquals("b", l.get(1));
-		assertEquals("c", l.get(2));
-		assertEquals("d", l.get(3));
-		assertEquals(4, l.size());
+		l.add("b");
+		l.add("c");
+		List<String> sl = l.subList(0, 2);
+		assertEquals(2, sl.size());
+		assertEquals("a", sl.get(0));
+		ListIterator<String> i = sl.listIterator();
+		assertEquals("a", i.next());
+		assertFalse(i.hasNext());
+		i.remove();
+		assertEquals("b", l.get(0));
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testThrowsIndexOutOfBoundsExceptionOnGet() {
+		l.get(1);
 	}
 
 	@Test
@@ -216,251 +556,10 @@ public class SuperListTest {
 	}
 
 	@Test
-	public void testRetainAll() {
-		l.add("a");
-		l.add("b");
-		l.add("c");
-		l.add("d");
-		List<String> c = new ArrayList<String>();
-		c.add("b");
-		c.add("c");
-		assertTrue(l.retainAll(c));
-		assertFalse(l.retainAll(c));
-		assertEquals("b", l.get(0));
-		assertEquals("c", l.get(1));
-		assertEquals(2, l.size());
-	}
-
-	@Test
-	public void testIteratorHasNext() {
-		l.add("a");
-		l.add("b");
-		assertTrue(l.listIterator().hasNext());
-		assertTrue(l.listIterator(1).hasNext());
-		assertFalse(l.listIterator(2).hasNext());
-	}
-
-	@Test
-	public void testIteratorHasPrevious() {
-		l.add("a");
-		l.add("b");
-		assertFalse(l.listIterator().hasPrevious());
-		assertTrue(l.listIterator(1).hasPrevious());
-		assertTrue(l.listIterator(2).hasPrevious());
-	}
-
-	@Test
-	public void testIteratorNextIndex() {
-		l.add("a");
-		l.add("b");
-		assertEquals(1, l.listIterator().nextIndex());
-		assertEquals(2, l.listIterator(1).nextIndex());
-	}
-
-	@Test
-	public void testIteratorPreviousIndex() {
-		l.add("a");
-		l.add("b");
-		assertEquals(-1, l.listIterator().previousIndex());
-		assertEquals(0, l.listIterator(1).previousIndex());
-		assertEquals(1, l.listIterator(2).previousIndex());
-	}
-
-	@Test
-	public void testIteratorNext() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator();
-		assertEquals("a", i.next());
-		assertTrue(i.hasNext());
-		assertEquals("b", i.next());
-		assertEquals(3, i.nextIndex());
-		assertFalse(i.hasNext());
-		try {
-			i.next();
-			fail("i.next() should throw exception!");
-		} catch (NoSuchElementException e) {
-		}
-	}
-
-	@Test
-	public void testIteratorPrevious() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator(2);
-		assertEquals("b", i.previous());
-		assertTrue(i.hasPrevious());
-		assertEquals("a", i.previous());
-		assertEquals(-1, i.previousIndex());
-		assertFalse(i.hasPrevious());
-		try {
-			i.previous();
-			fail("i.previous() schould throw exception!");
-		} catch (NoSuchElementException e) {
-		}
-	}
-
-	@Test
-	public void testIteratorNextPrevious() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator(2);
-		assertEquals("b", i.previous());
-		assertTrue(i.hasPrevious());
-		assertEquals("a", i.previous());
-		assertTrue(i.hasNext());
-		assertEquals("a", i.next());
-		assertEquals("b", i.next());
-	}
-
-	@Test
-	public void testIteratorSet() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator();
-		i.next();
-		i.set("c");
-		assertEquals("c", l.get(0));
-	}
-
-	@Test(expected = IllegalStateException.class)
-	public void testIteratorSetException() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator();
-		i.set("c");
-	}
-
-	@Test
-	public void testIteratorRemove() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator();
-		i.next();
-		i.remove();
-		assertEquals(0, i.nextIndex());
-		assertEquals("b", l.get(0));
-		assertEquals("b", i.next());
-		assertEquals(1, i.nextIndex());
-	}
-
-	@Test(expected = IllegalStateException.class)
-	public void testIteratorRemoveException() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator();
-		i.remove();
-	}
-
-	@Test
-	public void testIteratorAdd() {
-		l.add("a");
-		l.add("b");
-		ListIterator<String> i = l.listIterator();
-		i.add("c");
-		i.next();
-		i.add("d");
-		assertEquals(4, i.nextIndex());
-		assertEquals("d", i.previous());
-		assertEquals("c", l.get(0));
-		assertEquals("a", l.get(1));
-		assertEquals("d", l.get(2));
-		assertEquals("b", l.get(3));
-	}
-
-	@Test
 	public void testToString() {
 		l.add("a");
 		l.add("b");
 		l.add("c");
 		assertEquals("[a, b, c]", l.toString());
-	}
-
-	@Test
-	public void testEquals() {
-		l.add("a");
-		l.add("b");
-		l.add("c");
-		List<String> c = new ArrayList<String>();
-		c.add("a");
-		c.add("b");
-		assertFalse(l.equals(c));
-		assertFalse(c.equals(l));
-		c.add("c");
-		assertTrue(l.equals(c));
-		assertTrue(c.equals(l));
-	}
-
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testClone() throws CloneNotSupportedException {
-		l.add("a");
-		l.add("b");
-		SuperList<String> l2 = (SuperList<String>) l.clone();
-		l2.add("c");
-		assertEquals(2, l.size());
-	}
-
-	@Test
-	public void testSublist() {
-		l.add("a");
-		l.add("b");
-		l.add("c");
-		List<String> sl = l.subList(0, 2);
-		assertEquals(2, sl.size());
-		assertEquals("a", sl.get(0));
-		ListIterator<String> i = sl.listIterator();
-		assertEquals("a", i.next());
-		assertFalse(i.hasNext());
-		i.remove();
-		assertEquals("b", l.get(0));
-	}
-
-	@Test
-	public void testElement() {
-		try {
-			l.element();
-			fail("l.element() should throw excepton!");
-		} catch (NoSuchElementException e) {
-		}
-		l.add("a");
-		l.add("b");
-		assertEquals("a", l.element());
-	}
-
-	@Test
-	public void testOffer() {
-		assertTrue(l.offer("a"));
-		assertEquals("a", l.get(0));
-		assertEquals(1, l.size());
-	}
-
-	@Test
-	public void testPeek() {
-		assertNull(l.peek());
-		l.add("a");
-		l.add("b");
-		assertEquals("a", l.peek());
-	}
-
-	@Test
-	public void testPoll() {
-		l.add("a");
-		l.add("b");
-		assertEquals("a", l.poll());
-		assertEquals("b", l.poll());
-		assertNull(l.poll());
-	}
-
-	@Test
-	public void testRemove() {
-		l.add("a");
-		l.add("b");
-		assertEquals("a", l.remove());
-		assertEquals("b", l.remove());
-		try {
-			l.remove();
-		} catch (NoSuchElementException e) {
-		}
 	}
 }
